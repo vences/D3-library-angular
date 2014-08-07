e7816b295248bb4ea7f024172132b8229a7675c0
vences <onevan.mail@gmail.com>
2014-08-06 21:45:54 -0400

    remove from gitignore public folder
    I put this folder in gitignore because I would like to publish on
    gh-pages. But if I add new files is not take in consideration. That’s
    why I remove from gitignore

---
+++ public/scripts/controllers/barChart.js
@@ -0,0 +1,13 @@
+angular.module('app')
+.controller('barChartController', function ($scope, $location) {
+  $scope.options = {
+    width: 500, 
+    height: 500
+  };
+  $scope.data = [1, 2, 3, 4];
+  $scope.hovered = function(d){
+      $scope.barValue = d;
+      $scope.$apply();
+  };
+  $scope.barValue = 'None';
+});
